32d1233a043b2112fb72fe85572b820062820667
marskingx <shamangels@gmail.com>
2019-11-24 20:42:17 +0800

    v0.2 篩選功能

--- read/reviews.py
+++ read/reviews.py
@@ -19,3 +19,9 @@ for d in data:
     sum_len += len(d)
     print(sum_len)
 print('平均是',sum_len/len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print("一共有", len(new), "筆留言小於100個單字")
