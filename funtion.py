d9de69f0c51efa5aa1078602c5efa65be03b3659
Mars_Lee <marskingx@gmail.com>
2019-12-19 20:42:15 +0800

    v0.1.2 新增function練習

--- funtion.py
+++ funtion.py
@@ -2,15 +2,14 @@
 # function 是用來【收納】程式碼的
 # 它是個功能
 
-def wash(dry):
-    print('加水')
+def wash(dry=False, water=8):
+    print('加水', water ,'分滿')
     print('加洗衣精')
     print('旋轉')
     if dry:
         print('烘衣')
 
-wash(False)
-wash(True)  # 使用function
+wash(water=10)  # 使用function
 
 
 def say_hi():
@@ -19,8 +18,7 @@ def say_hi():
 
 say_hi()
 
-def add(x, y):
+def add(x, y=1):
     print(x + y)
-add(3, 4)
-add(5, 5)
+add(3, 2)
 
