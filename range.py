3a5fca805793e10b24e14665fdbaca27f02ce133
marskingx <shamangels@gmail.com>
2019-12-25 23:24:55 +0800

    v0.5 修正random.py帶來的錯誤

--- range.py
+++ range.py
@@ -6,6 +6,5 @@ import random
 range(5)
 range(3)
 for i in range(100):
-    r = random.randint(1, 100)
-    print('hi')
-
+    r = random.randint(1, 1000)
+    print('這是第', i+1, '次產生隨機數：', r)
--- read/reviews.py
+++ read/reviews.py
@@ -14,36 +14,60 @@ print('------------------')
 print((data[1]))
 print('檔案讀完了，總共有', len(data), '筆資料')
 
-sum_len = 0
+print([0])
+wc = {}  # 文字計數
 for d in data:
-    sum_len += len(d)
-    print(sum_len)
-print('平均是', sum_len / len(data))
+    words = d.split(' ')
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1  # 新增新的key進入wc字典
 
-new = []
-for d in data:
-    if len(d) < 100:
-        new.append(d)
-print("一共有", len(new), "筆留言小於100個單字")
-print(new[0])
-print(new[1])
+for word in wc:
+    if wc[word] > 100000000:
+        print(word, wc[word])
+print(len(wc))
+print(wc['Allen'])
 
-good = []
-for d in data:
-    if 'good' in d:
-        good.append(d)
-print('一共有', len(good), '筆資料有含GOOD')
-print(good[0])
+while True:
+    input('請問你想查什麼字?')
+    if word == 'Q':
+        break
+    print(word, '出現過的次數為：', wc[word])
 
-good = [d for d in data if 'good' in d]
-print(len(good))
+print('感謝使用本查詢功能')
 
-test = []
-for t in data:
-    if 'test' in t:
-        good.append(t)
-print('一共有', len(test), '筆資料有含test')
-print(test)
-
-test = [t for t in data if 'test' in t]
-print(test)
+# sum_len = 0
+# for d in data:
+#     sum_len += len(d)
+#     print(sum_len)
+# print('平均是', sum_len / len(data))
+#
+# new = []
+# for d in data:
+#     if len(d) < 100:
+#         new.append(d)
+# print("一共有", len(new), "筆留言小於100個單字")
+# print(new[0])
+# print(new[1])
+#
+# good = []
+# for d in data:
+#     if 'good' in d:
+#         good.append(d)
+# print('一共有', len(good), '筆資料有含GOOD')
+# print(good[0])
+#
+# good = [d for d in data if 'good' in d]
+# print(len(good))
+#
+# test = []
+# for t in data:
+#     if 'test' in t:
+#         good.append(t)
+# print('一共有', len(test), '筆資料有含test')
+# print(test)
+#
+# test = [t for t in data if 'test' in t]
+# print(test)
